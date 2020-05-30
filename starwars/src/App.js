f5e02f30b96132ead28e0f521cd50dbdaca0d892
Jason <47641725+Jfadelli@users.noreply.github.com>
2020-05-29 19:20:24 -0700

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import CharacterComp from './components/Character'
 
+// Try to think through what state you'll need for this app before starting. Then build out
+// the state properties here.
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [characters, setCharacaters] = useState([])
+
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character')
+      .then(response => {
+        setCharacaters(response.data.results)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <div className='App'>
+      <CharacterComp characterData={characters} />
+    </div >
   );
 }
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,44 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const BodyDiv = styled.div`
+display: flex;
+flex-direction: row;
+flex-flow: wrap;
+margin: auto 0
+width: 100%;
+`
+const StyledDiv = styled.div`
+width: 20em;
+border: 2px solid gray;
+border-radius: 10px;
+margin: 2vh 2vh;
+background-color: #e6d3a1;
+`
+const StyledImg = styled.img`
+border: 1px solid black;
+width: 200px;
+height: 275px;
+margin: 2vh;
+box-shadow: 0 10px 20px 2px #ad985f;
+
+`
+
+const characterComp = (props) => {
+    return (
+        <BodyDiv>
+            {props.characterData.map(character => {
+                return (
+                    <StyledDiv key={character.id}>
+                        <h2>{character.name}</h2>
+                        <p>{character.status}</p>
+                        <StyledImg alt={character.name + 'face'} src={character.image} />
+                    </StyledDiv>
+                )
+            })}
+        </BodyDiv >
+    )
+}
+
+export default characterComp
